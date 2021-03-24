3ac336817f13c5158a3dae95a281baa8cd1150d5
alvarojguerrero <guerreroalvarojavier@gmail.com>
2021-03-23 21:55:51 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -8,7 +8,9 @@ public class Persona {
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+    }
+    Persona(){
+        this.cedula = 0; 
     }
 
     Persona(long cedula, String nombre) {
@@ -31,6 +33,7 @@ public class Persona {
 
     Persona(String nombre) {
         this.nombre = "";
+        this.cedula=1;
         totalPersonas++;
     }
     
